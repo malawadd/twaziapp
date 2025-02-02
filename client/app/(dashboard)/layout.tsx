cba61682eb643b547a681c8b23732d282852957a
malawad <50057882+malawadd@users.noreply.github.com>
2025-02-02 09:28:36 +0300

    ⭐ added darkmode ⭐

--- client/app/(dashboard)/layout.tsx
+++ client/app/(dashboard)/layout.tsx
@@ -1,4 +1,7 @@
+import BreadcrumbHeader from "@/components/BreadcrumbHeader";
 import DesktopSidebar from "@/components/Sidebar";
+import { ModeToggle } from "@/components/ThemeModeToggle";
+
 import { Separator } from "@/components/ui/separator";
 import React from "react";
 
@@ -8,7 +11,10 @@ function layout({ children }: { children: React.ReactNode }) {
       <DesktopSidebar />
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
-          Twazi
+          <BreadcrumbHeader />
+          <div className="gap-1 flex items-center">
+            <ModeToggle/>
+          </div>
         </header>
         <Separator />
         <div className="overflow-auto">
