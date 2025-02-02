cac68a5836926dd60335a455cfcc1b63edb24492
malawad <50057882+malawadd@users.noreply.github.com>
2025-02-02 15:56:45 +0300

    🍇 added workflow page 🍇

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -33,7 +33,9 @@ const routes = [
 function DesktopSidebar() {
     const pathname = usePathname();
     const { address } = useAccount(); // Track wallet connection
-    const activeRoute = routes.find((route) => pathname.includes(route.href)) || routes[0];
+    // const activeRoute = routes.find((route) => pathname.includes(route.href)) || routes[0];
+    const activeRoute = routes.find((route) => pathname === route.href) || routes[0];
+
 
     return (
         <div className="hidden md:flex flex-col min-w-[280px] max-w-[280px] h-screen bg-primary/5 dark:bg-secondary/30 border-r-4 border-black ">
@@ -66,16 +68,16 @@ function DesktopSidebar() {
             )}
 
             {address && (
-  <div className="flex flex-col items-center mt-auto p-4 border-t-4 border-black shadow-[4px_4px_0px_black] relative">
+                <div className="flex flex-col items-center mt-auto p-4 border-t-4 border-black shadow-[4px_4px_0px_black] relative">
 
-<Wallet className="w-full flex flex-col items-center relative">
+                    <Wallet className="w-full flex flex-col items-center relative">
                         <ConnectWallet className="p-3 border-4 border-black shadow-[4px_4px_0px_black] bg-[#a3e636] hover:bg-[#a3e636] flex items-center gap-2   hover:shadow-none transition-all ">
 
-                            
+
                             <Name className='text-black' />
                         </ConnectWallet>
-                        <WalletDropdown         className="absolute bottom-full mb-2 w-4 bg-black text-white border-4 border-black shadow-[4px_4px_0px_black] rounded-md z-50"
->
+                        <WalletDropdown className="absolute bottom-full mb-2 w-4 bg-black text-white border-4 border-black shadow-[4px_4px_0px_black] rounded-md z-50"
+                        >
                             <Identity
                                 className="px-4 pt-3 pb-2 hover:bg-[#beee70]"
                                 hasCopyAddressOnClick
@@ -85,7 +87,7 @@ function DesktopSidebar() {
                                 <Address />
                                 <EthBalance />
                             </Identity>
-                            
+
                             <WalletDropdownDisconnect className='hover:bg-[#beee70]' />
                         </WalletDropdown>
                     </Wallet>
@@ -103,8 +105,8 @@ export function MobileSidebar() {
 
     return (
         <div className="block bg-background md:hidden ">
-           <nav className="flex items-center justify-between p-4 border-b-1 border-black">
-                
+            <nav className="flex items-center justify-between p-4 border-b-1 border-black">
+
                 <Sheet open={isOpen} onOpenChange={setOpen}>
                     <SheetTrigger asChild>
                         <Button variant="ghost" size="icon" className="border-4 border-black">
@@ -117,10 +119,10 @@ export function MobileSidebar() {
                     >
                         {address ? (
                             <>
-                             <div className="flex items-center justify-between">
-                            <Logo />
+                                <div className="flex items-center justify-between">
+                                    <Logo />
 
-                        </div>
+                                </div>
                                 <div className="flex flex-col gap-4">
                                     {routes.map((route) => (
                                         <Link
@@ -137,25 +139,25 @@ export function MobileSidebar() {
                                     ))}
                                 </div>
                                 <Wallet className=" flex items-center flex-auto flex-col">
-                        <ConnectWallet className="p-3 border-4 border-black shadow-[4px_4px_0px_black] bg-[#a3e636] hover:bg-[#a3e636] flex items-center gap-2   hover:shadow-none transition-all ">
+                                    <ConnectWallet className="p-3 border-4 border-black shadow-[4px_4px_0px_black] bg-[#a3e636] hover:bg-[#a3e636] flex items-center gap-2   hover:shadow-none transition-all ">
 
-                            <Avatar className="h-6 w-6" />
-                            <Name className='text-black' />
-                        </ConnectWallet>
-                        <WalletDropdown className="relative">
-                            <Identity
-                                className="px-4 pt-3 pb-2 hover:bg-[#beee70]"
-                                hasCopyAddressOnClick
-                            >
-                                <Avatar />
-                                <Name />
-                                <Address />
-                                <EthBalance />
-                            </Identity>
-                            
-                            <WalletDropdownDisconnect className='hover:bg-[#beee70]' />
-                        </WalletDropdown>
-                    </Wallet>
+                                        <Avatar className="h-6 w-6" />
+                                        <Name className='text-black' />
+                                    </ConnectWallet>
+                                    <WalletDropdown className="relative">
+                                        <Identity
+                                            className="px-4 pt-3 pb-2 hover:bg-[#beee70]"
+                                            hasCopyAddressOnClick
+                                        >
+                                            <Avatar />
+                                            <Name />
+                                            <Address />
+                                            <EthBalance />
+                                        </Identity>
+
+                                        <WalletDropdownDisconnect className='hover:bg-[#beee70]' />
+                                    </WalletDropdown>
+                                </Wallet>
                             </>
                         ) : (
                             <div className="flex-1 flex items-center justify-center">
