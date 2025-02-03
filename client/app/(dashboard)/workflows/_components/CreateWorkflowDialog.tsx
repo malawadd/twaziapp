0a81ba53cd6d5322ab99da806ad2d016b23633e2
malawad <50057882+malawadd@users.noreply.github.com>
2025-02-03 13:52:24 +0300

    🍅 adding form 🍅

--- client/app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
+++ client/app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -5,9 +5,20 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Layers2Icon } from "lucide-react";
 import CustomDialogHeader from "@/components/CustomDialogHeader";
+import { createWorkflowSchema } from "@/schema/workflow";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 
 function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
   const [open, setOpen] = useState(false);
+  const form = useForm<z.infer<typeof createWorkflowSchema>>({
+    resolver: zodResolver(createWorkflowSchema),
+    defaultValues: {},
+  });
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -16,10 +27,37 @@ function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
       </DialogTrigger>
       <DialogContent className="px-0">
         <CustomDialogHeader
-        icon={Layers2Icon}
-        title="Create Workflow"
-        subTitle="Start Building agents workflows"/>
-        
+          icon={Layers2Icon}
+          title="Create Workflow"
+          subTitle="Start Building agents workflows" />
+
+        <div className="p-6">
+          <Form {...form}>
+            <form className="space-y-8 w-full">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex gap-1 items-center">
+                      Name
+                      <p className="text-xs text-primary">(required)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Choose a descriptive and unique name
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
+
+
       </DialogContent>
 
     </Dialog>
