2981faf9bc881b3f02ef7e2e05a5461bc914865e
malawad <50057882+malawadd@users.noreply.github.com>
2025-02-04 12:02:29 +0300

    🍦 added workflow editor 🍦

---
+++ client/app/workflow/_components/FlowEditor.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Workflow } from "@prisma/client";
+import { Background, BackgroundVariant, Controls, ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
+import React from "react";
+import "@xyflow/react/dist/style.css";
+
+
+function FlowEditor({ workflow }: { workflow: Workflow }) {
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  return (
+    <main className="h-full w-full">
+      <ReactFlow 
+      nodes={nodes}
+      edges={edges}
+      onEdgesChange={onEdgesChange}
+      onNodesChange={onNodesChange}/>
+      <Controls position="top-left" />
+      <Background variant={BackgroundVariant.Dots} gap={12} />
+    </main>
+  );
+}
+
+export default FlowEditor;
--- client/app/workflow/editor/[workflowId]/page.tsx
+++ client/app/workflow/editor/[workflowId]/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { cookies } from "next/headers";
 import prisma from "@/lib/prisma";
+import Editor from "../../_components/Editor";
 
 async function page({ params }: { params: { workflowId: string } }) {
   const { workflowId } = params;
@@ -19,7 +20,7 @@ async function page({ params }: { params: { workflowId: string } }) {
     return <div>Workflow not found</div>;
   }
 
-  return <pre >{JSON.stringify(workflow, null, 4)}</pre>;
+  return <Editor workflow={workflow}/>;
 }
 
 export default page;
