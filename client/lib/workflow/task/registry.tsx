f3f7901fbe0b5a677d884be51b8ee293ceac95c1
malawad <50057882+malawadd@users.noreply.github.com>
2025-02-05 14:28:51 +0300

    🥗 created new node 🥗

--- client/lib/workflow/task/registry.tsx
+++ client/lib/workflow/task/registry.tsx
@@ -2,10 +2,15 @@
 import { LaunchBrowserTask } from "@/lib/workflow/task/LaunchBrowser";
 
 import { TaskType } from "@/types/task";
+import { PageToHtmlTask } from "./PageToHtml";
+import { WorkflowTask } from "@/types/workflow";
 
-
+type Registry = {
+  [K in TaskType]: WorkflowTask & { type: K };
+};
 
 export const TaskRegistry = {
   LAUNCH_BROWSER: LaunchBrowserTask,
+  PAGE_TO_HTML: PageToHtmlTask,
   
 };
--- client/types/workflow.ts
+++ client/types/workflow.ts
@@ -1,4 +1,17 @@
+import { TaskParam, TaskType } from "@/types/task";
+import { LucideProps } from "lucide-react";
+
 export enum WorkflowStatus {
     DRAFT = "DRAFT",
     PUBLISHED = "PUBLISHED",
 }
+
+export type WorkflowTask = {
+    label: string;
+    icon: React.FC<LucideProps>;
+    type: TaskType;
+    isEntryPoint?: boolean;
+    inputs: TaskParam[];
+    outputs: TaskParam[];
+    credits: number;
+  };
