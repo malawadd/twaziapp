60d9b4b43e86c6b3812ab2a3a443a911d2f3cef4
malawad <50057882+malawadd@users.noreply.github.com>
2025-02-05 14:10:54 +0300

    🍟 reading workflows from server 🍟

--- client/app/workflow/_components/FlowEditor.tsx
+++ client/app/workflow/_components/FlowEditor.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Workflow } from "@prisma/client";
-import { Background, BackgroundVariant, Controls, ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
-import React from "react";
+import { Background, BackgroundVariant, Controls, ReactFlow, useEdgesState, useNodesState, useReactFlow } from "@xyflow/react";
+import React, { useEffect } from "react";
 import "@xyflow/react/dist/style.css";
 import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
 import { TaskType } from "@/types/task";
@@ -15,10 +15,21 @@ const nodeTypes = {
   const fitViewOptions = { padding: 1 };
 function FlowEditor({ workflow }: { workflow: Workflow }) {
   const [nodes, setNodes, onNodesChange] = useNodesState([
-        CreateFlowNode(TaskType.LAUNCH_BROWSER)
-    
   ]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const { setViewport, screenToFlowPosition, updateNodeData } = useReactFlow();
+
+  useEffect(() => {
+    try {
+      const flow = JSON.parse(workflow.definition);
+      if (!flow) return;
+      setNodes(flow.nodes || []);
+      setEdges(flow.edges || []);
+      if (!flow.viewport) return;
+      const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+      setViewport({ x, y, zoom });
+    } catch (error) {}
+  }, [workflow.definition, setEdges, setNodes, setViewport]);
 
   return (
     <main className="h-full w-full">
