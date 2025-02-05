fa2fb8df50ae891b136cae35871d0e2916985569
malawad <50057882+malawadd@users.noreply.github.com>
2025-02-05 17:03:59 +0300

    🧈 new task 🧈

--- client/app/workflow/_components/TaskMenu.tsx
+++ client/app/workflow/_components/TaskMenu.tsx
@@ -35,7 +35,7 @@ export default function TaskMenu() {
 
           <AccordionContent className="flex flex-col gap-1">
             <TaskMenuBtn taskType={TaskType.PAGE_TO_HTML} />
-
+            <TaskMenuBtn taskType={TaskType.EXTRACT_TEXT_FROM_ELEMENT} />
           </AccordionContent>
         </AccordionItem>
 
--- client/lib/workflow/task/registry.tsx
+++ client/lib/workflow/task/registry.tsx
@@ -4,6 +4,7 @@ import { LaunchBrowserTask } from "@/lib/workflow/task/LaunchBrowser";
 import { TaskType } from "@/types/task";
 import { PageToHtmlTask } from "./PageToHtml";
 import { WorkflowTask } from "@/types/workflow";
+import { ExtractTextFromElementTask } from "./ExtractTextFromElement";
 
 type Registry = {
   [K in TaskType]: WorkflowTask & { type: K };
@@ -12,5 +13,6 @@ type Registry = {
 export const TaskRegistry = {
   LAUNCH_BROWSER: LaunchBrowserTask,
   PAGE_TO_HTML: PageToHtmlTask,
+  EXTRACT_TEXT_FROM_ELEMENT: ExtractTextFromElementTask,
   
 };
