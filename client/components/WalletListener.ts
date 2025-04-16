f41eadae335b197cc53aaa76414398b74f0c6be0
malawad <50057882+malawadd@users.noreply.github.com>
2025-04-16 20:38:20 +0300

    wallet listnere now update the page

--- client/components/WalletListener.ts
+++ client/components/WalletListener.ts
@@ -1,9 +1,11 @@
 "use client";
 import { useAccount } from "wagmi";
 import { useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
 
 export default function WalletListener() {
   const { address, isConnected } = useAccount();
+  const router = useRouter();
   // Explicitly type the ref to accept boolean or null.
   const prevIsConnected = useRef<boolean | null>(null);
 
@@ -16,6 +18,8 @@ export default function WalletListener() {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ address }),
+        }).then(() => {
+          router.refresh();
         });
       }
       return;
@@ -27,6 +31,8 @@ export default function WalletListener() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ address }),
+      }).then(() => {
+        router.refresh();
       });
     }
 
@@ -36,12 +42,14 @@ export default function WalletListener() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ address: "" }),
+      }).then(() => {
+        router.refresh();
       });
     }
 
     // Update the previous state for next comparison
     prevIsConnected.current = isConnected;
-  }, [address, isConnected]);
+  }, [address, isConnected, router]);
 
   return null; // No UI rendered
 }
