b7f57b5395167daa5f239dc3580ca57c5aab4742
malawad <50057882+malawadd@users.noreply.github.com>
2025-04-16 12:28:04 +0300

    updated the button not showing at the bottom

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -39,16 +39,16 @@ function DesktopSidebar() {
 
 
     return (
-        <div className=" flex-col min-w-[280px] max-w-[280px] h-screen bg-primary/5 dark:bg-secondary/30 border-r-4 border-black ">
+        <div className="hidden md:flex flex-col min-w-[280px] max-w-[280px] h-screen bg-primary/5 dark:bg-secondary/30 border-r-4 border-black ">
             <div className="flex items-center justify-center p-4 border-b-4 border-black">
                 <Logo />
             </div>
             <TailwindPreserveSidebarStyles />
 
             {address ? (
-                <>
+                <div className="flex flex-col flex-1">
                     <div className="p-2 font-bold text-center border-b-4 border-black">TODO CREDIT</div>
-                    <div className="flex flex-col p-2 gap-4">
+                    <div className="flex flex-col p-2 gap-4 flex-1">
                         {routes.map((route) => (
                             <Link
                                 key={route.href}
@@ -62,56 +62,50 @@ function DesktopSidebar() {
                             </Link>
                         ))}
                     </div>
-                </>
-            ) : (
-                <div className="flex-1 flex items-center justify-center">
-                <ConnectWallet
-                  className="
-                    !p-4 
-                    !border-4 
-                    !border-black 
-                    !shadow-[4px_4px_0px_black] 
-                    !bg-red-500 
-                    !text-black 
-                    hover:text-[#1c2511] 
-                    !rounded-none 
-                    !font-semibold 
-                    !w-full 
-                    !justify-center 
-                    transition-all 
-                    duration-200 
-                    hover:!shadow-none 
-                    hover:!bg-[#beee70]
-                  "
-                />
-              </div>
-            )}
 
-            {address && (
-                <div className="flex flex-col items-center mt-auto p-4 border-t-4 border-black shadow-[4px_4px_0px_black] relative">
-
-                    <Wallet className="w-full flex flex-col items-center relative">
-                        <ConnectWallet className="p-3 border-4 border-black shadow-[4px_4px_0px_black] bg-[#a3e636] hover:bg-[#a3e636] flex items-center gap-2   hover:shadow-none transition-all ">
-
-
-                            <Name className='text-black' />
-                        </ConnectWallet>
-                        <WalletDropdown className="absolute bottom-full mb-2 w-4 bg-black text-white border-4 border-black shadow-[4px_4px_0px_black] rounded-md z-50"
-                        >
-                            <Identity
-                                className="px-4 pt-3 pb-2 hover:bg-[#beee70]"
-                                hasCopyAddressOnClick
+                    <div className="flex flex-col items-center mt-auto p-4 border-t-4 border-black shadow-[4px_4px_0px_black] relative">
+                        <Wallet className="w-full flex flex-col items-center relative">
+                            <ConnectWallet className="p-3 border-4 border-black shadow-[4px_4px_0px_black] bg-[#a3e636] hover:bg-[#a3e636] flex items-center gap-2   hover:shadow-none transition-all ">
+                                <Name className='text-black' />
+                            </ConnectWallet>
+                            <WalletDropdown className="absolute bottom-full mb-2 w-4 bg-black text-white border-4 border-black shadow-[4px_4px_0px_black] rounded-md z-50"
                             >
-                                <Avatar />
-                                <Name />
-                                <Address />
-                                <EthBalance />
-                            </Identity>
+                                <Identity
+                                    className="px-4 pt-3 pb-2 hover:bg-[#beee70]"
+                                    hasCopyAddressOnClick
+                                >
+                                    <Avatar />
+                                    <Name />
+                                    <Address />
+                                    <EthBalance />
+                                </Identity>
 
-                            <WalletDropdownDisconnect className='hover:bg-[#beee70]' />
-                        </WalletDropdown>
-                    </Wallet>
+                                <WalletDropdownDisconnect className='hover:bg-[#beee70]' />
+                            </WalletDropdown>
+                        </Wallet>
+                    </div>
                 </div>
+            ) : (
+                <div className="flex-1 flex items-center justify-center h-full">
+  <ConnectWallet
+    className="
+      !p-4 
+      !border-4 
+      !border-black 
+      !shadow-[4px_4px_0px_black] 
+      !bg-red-500 
+      !text-black 
+      hover:text-[#1c2511] 
+      !rounded-none 
+      !font-semibold 
+      !w-full 
+      !justify-center 
+      transition-all 
+      duration-200 
+      hover:!shadow-none 
+      hover:!bg-[#beee70]"
+  />
+</div>
             )}
         </div>
     );
